dd892da2cc11dfcbbf2515d2e41e4ae900ee7571
niceonionslice <niceonionslice@gmail.com>
2019-08-07 22:33:23 +0900

    design: cssを適用するために追加

--- my-app/src/TodoInput.js
+++ my-app/src/TodoInput.js
@@ -4,7 +4,7 @@ class TodoInput extends Component {
   render() {
     return (
       <div>
-        <input placeholder='新規TODOを入力してください' />
+        <input placeholder='新規TODOを入力してください' className='todoinput-form__input' />
         <button>登録</button>
       </div>
     );
