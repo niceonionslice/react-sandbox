d2dbc13138741d4a64f918810538e77325d01d18
niceonionslice <niceonionslice@gmail.com>
2019-08-07 16:10:29 +0900

    APPコンポーネントにHelloコンポーネントを追記

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 class App extends Component {
   render() {
@@ -8,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello World!</h2>
+          <Hello name="坂本龍馬" />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
