3b5b4b9e1bd39b4bde24220f79c55a877b6c3809
niceonionslice <niceonionslice@gmail.com>
2019-08-07 17:29:38 +0900

    change APPコンポーネントにTodoコンポーネントの描画を実装

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+
+class App extends Component {
+  render() {
+    const tasks = [
+      { title: 'Todo1つ目', id:0 },
+      { title: 'Todo2つ目', id:1 },
+    ];
+
+    return (
+      <div>
+        <h1>TODO App</h1>
+        <TodoInput />
+        <TodoList tasks={tasks} />
+      </div>
+    );
+  }
+}
+
+export default App;
